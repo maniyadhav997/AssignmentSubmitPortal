e23631982bb45ab98997e3e18d58bc79d69861e7
Manikanta <119345417+maniyadhav997@users.noreply.github.com>
2024-11-16 22:56:57 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const { validateRegister, validateLogin, handleValidationErrors } = require('../validators/authValidators');
+const { validateAssignmentUpload, handleAssignmentValidationErrors } = require('../validators/assignmentValidators');
 const { registerAdmin, loginAdmin, viewAssignments, updateAssignmentStatus } = require('../controllers/adminController');
 const { authenticateAdmin } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/register', registerAdmin);
-router.post('/login', loginAdmin);
+router.post('/register', validateRegister, handleValidationErrors, registerAdmin);
+router.post('/login', validateLogin, handleValidationErrors, loginAdmin);
 router.get('/assignments', authenticateAdmin, viewAssignments);
 router.post('/assignments/:id/:action', authenticateAdmin, updateAssignmentStatus);
 
 module.exports = router;
+
