337a152c3ef89e2147a3fd626f567eb630a13154
Manikanta <119345417+maniyadhav997@users.noreply.github.com>
2024-11-16 22:57:35 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const { validateRegister, validateLogin, handleValidationErrors } = require('../validators/authValidators');
 const { registerUser, loginUser, uploadAssignment, getAllAdmins } = require('../controllers/userController');
 const { authenticateUser } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.post('/upload', authenticateUser, uploadAssignment);
+// Apply the validation middleware 
+router.post('/register', validateRegister, handleValidationErrors, registerUser);
+router.post('/login', validateLogin, handleValidationErrors, loginUser);
+router.post('/upload', authenticateUser, validateAssignmentUpload, handleAssignmentValidationErrors, uploadAssignment);
 router.get('/admins', authenticateUser, getAllAdmins);
 
 module.exports = router;
